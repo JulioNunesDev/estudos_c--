380bd92b356f0edda22e3ef1bf9edb7ceeea5cd6
JulioNunesDev <julionunes999@gmail.com>
2022-07-20 12:07:41 -0300

    feat: dec_inc

---
+++ Inc_Dec.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int n1, n2;
+    n1 = 0;
+    n2 = 10;
+
+    cout << "valor n1 antes: " << n1 << endl;
+    // n1 = n1 + 1 First form simple
+    // n1 += 1 Second form simple
+    n1++;  // n1 += 1 Three form simple
+
+    cout << "valor n1 depois: " << n1 << endl;
+
+    cout << "valor n1 antes decrement: " << n1 << endl;
+
+    n1--;
+
+    cout << "valor n1 depois decrement: " << n1 << endl;
+
+
+
+
+
+    return 0;
+}
