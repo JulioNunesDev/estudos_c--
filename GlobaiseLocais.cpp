b0cce298f6c50c2e4341044d5181891551f83496
JulioNunesDev <julionunes999@gmail.com>
2022-07-20 11:21:11 -0300

    .

--- GlobaiseLocais.cpp
+++ GlobaiseLocais.cpp
@@ -6,7 +6,18 @@ int n1, n2;   //Variáveis Globais
 
 int main()
 {
+    // Operadores matemáticos: + - / * % ()
+
     int n3, n4; //Variáveis Locais
+    int res;
+    n1 = 11;
+    n2 = 8;
+    n3 = 4;
+    n4 = 5;
+
+    res = (n1 + n2 + n3 * n4) / 2 * 15 % 25;
+
+    cout << "O resultado e: " << res;
 
     return 0;
 }
