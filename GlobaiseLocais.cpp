6f8a438ad9599ac0c254fabf9d97020818924e00
JulioNunesDev <julionunes999@gmail.com>
2022-07-20 11:14:13 -0300

    feat: locais, globais

---
+++ GlobaiseLocais.cpp
@@ -0,0 +1,12 @@
+//Variáveis Globais e Locais, Operadores matemáticos
+#include <iostream>
+using namespace std;
+
+int n1, n2;   //Variáveis Globais
+
+int main()
+{
+    int n3, n4; //Variáveis Locais
+
+    return 0;
+}
