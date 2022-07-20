40559f2c040911b87bf0b0d43b58705cc53f822a
JulioNunesDev <julionunes999@gmail.com>
2022-07-20 11:07:38 -0300

    feat: c++

---
+++ declaracoes_multvariaveis.cpp
@@ -0,0 +1,15 @@
+//Declarações múltiplas de variáveis, Constantes:
+
+#include <iostream>
+using namespace std;
+
+#define log cout << "log de teste\n\n";
+//int life
+//int fire
+//int vel
+int main()
+{
+    int life, fire, vel;
+    log;;
+    return 0;
+}
---
+++ variáveis.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int vida;
+    char letra;
+    double deci = 2.5;
+    float deci2 = 2.4;
+    bool vivo = true;
+    string nome = "julio";
+    cout << "digite o valor: " ;
+    cin >> vida;
+    cout << "sua vida e: " << vida <<endl;
+    system("pause");
+    return 0;
+}
+ 
