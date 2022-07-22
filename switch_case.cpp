95d282de7f8a361a1ca3121dcfbe90a9b10643c9
JulioNunesDev <julionunes999@gmail.com>
2022-07-22 01:59:22 -0300

    ..

---
+++ switch_case.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int main(){
+
+    int age;
+
+    switch (age)
+    {
+    case 00:
+        std:: cout << "xxxx";
+        break;
+    case 11:
+        std:: cout << "yyyy";
+    default:
+       std::cout << "zzz";
+        break;
+    }
+    return 0;
+}
