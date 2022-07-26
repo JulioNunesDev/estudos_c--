2d5d326f1ce50eb44c9c01d54f8c755022410277
JulioNunesDev <julionunes999@gmail.com>
2022-07-26 10:07:18 -0300

    add: argc, argv

--- argc_argv.cpp
+++ argc_argv.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-
-int main()
+#include <string.h>
+int main(int argc, char* argv[])
 {
-    
+    if(argc > 1)
+    {
 
-    return 0;
+    if(!strcmp(argv[1], "Filmes")){
+        std:: cout << "[Lista de filmes] \n\n";
+    }else if(!strcmp(argv[1], "Jogo")){
+        std:: cout << "[Lista de jogos] \n\n";
+    }else{
+         std:: cout << "[Lista Vazia] \n\n";
+    }
+    }
+    
 }
