9333511b6985aa0d3a96ff3e0a1c4d3f3162949d
JulioNunesDev <julionunes999@gmail.com>
2022-07-26 11:42:42 -0300

    ft,fb

---
+++ fatorial.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+
+
+int fatorial(int n){
+    if(n > 1){
+    return n * fatorial(n - 1);
+    }
+    return n;
+}
+
+int fibonacci(int num){
+    if(num <= 1 || num == 2) return 1;
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+
+int main ()
+{
+  std::cout << "Fatorial de: " <<  fatorial(5) << "\n\n";
+ 
+ int val = 8;
+ for(int i = 0; i < val; i++)
+ {
+    std:: cout << fibonacci(i+1) << " ";
+ }
+
+    return 0;
+}
