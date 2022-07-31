29012e057069948142d1d1c6a5ad2ed9d50d19fb
JulioNunesDev <julionunes999@gmail.com>
2022-07-31 17:06:52 -0300

    ..

---
+++ printf&scanf.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main () {
+
+    int num = 10;
+    int num1;
+
+    printf("valor da variavel: %d", num);
+    scanf("%d",&num1);
+
+
+    return 0;
+}
